41591ae95a9003906f06f9c762871b12060bd4c7
coolia <gj@vfu56.com>
2018-01-24 11:34:12 +0800

    ImagePart

---
+++ src/assets/data/index.js
@@ -0,0 +1,69 @@
+const imgs = [
+	{
+		url: require('../images/1.jpg'),
+		width:'800',
+		height:'1120',
+		title:'高挑嫩模Dora纤细美腿丝袜姿态优雅动人写真'
+	},
+	{
+		url: require('../images/2.jpg'),
+		width:'800',
+		height:'540',
+		title:'测试图片'
+	},
+	{
+		url: require('../images/3.jpg'),
+		width:'800',
+		height:'1120',
+		title:'测试图片'
+	},
+	{
+		url: require('../images/4.jpg'),
+		width:'800',
+		height:'540',
+		title:'测试图片'
+	},
+	{
+		url: require('../images/5.jpg'),
+		width:'209',
+		height:'291',
+		title:'测试图片'
+	},
+	{
+		url: require('../images/6.jpg'),
+		width:'209',
+		height:'291',
+		title:'测试图片'
+	},
+	{
+		url: require('../images/7.jpg'),
+		width:'209',
+		height:'291',
+		title:'测试图片'
+	},
+	{
+		url: require('../images/8.jpg'),
+		width:'209',
+		height:'291',
+		title:'测试图片'
+	},
+	{
+		url: require('../images/9.jpg'),
+		width:'209',
+		height:'291',
+		title:'测试图片'
+	},
+	{
+		url: require('../images/10.jpg'),
+		width:'209',
+		height:'291',
+		title:'测试图片'
+	}
+]
+const data = {
+
+}
+export {
+	imgs,
+	data
+}
--- src/components/ImagePart/index.vue
+++ src/components/ImagePart/index.vue
@@ -1,19 +1,96 @@
 <template>
 	<div class="pad-top-36">
 		<tab-bar-top/>
-		<h1>图片区</h1>
-		<tab-bar-bottom/>
+		<div class="list clearfix">
+			<div class="left">
+				<div class="item" v-for="(leftItem, index) in imgData" v-if="index % 2 != 0">
+					<div class="itemBox">
+						<img :src="leftItem.url" />
+						<p class="title">新人嫩模杨漫妮居家私房秀傲人豪乳极致美臀诱惑</p>
+						<p class="otherInfo"><span class="fl"><svg-icon icon-class="eye"></svg-icon> 123</span><span class="fr"><svg-icon icon-class="like"></svg-icon> 123</span></p>
+					</div>
+				</div>
+			</div>
+			<div class="right">
+				<div class="item" v-for="(rightItem, index) in imgData" v-if="index % 2 == 0">
+					<div class="itemBox">
+						<img :src="rightItem.url" />
+						<p class="title">新人嫩模杨漫妮居家私房秀傲人豪乳极致美臀诱惑</p>
+						<p class="otherInfo"><span class="fl"><svg-icon icon-class="eye"></svg-icon> 123</span><span class="fr"><svg-icon icon-class="like"></svg-icon> 123</span></p>
+					</div>
+				</div>
+			</div>
+		</div>
+		<!--<tab-bar-bottom/> -->
 	</div>
 </template>
 <script type="text/javascript">
-	import TabBarTop from '../CommonComponents/TabBarTop'
-	import TabBarBottom from '../CommonComponents/TabBarBottom'
-	export default {
-		components: {
-			TabBarTop,
-			TabBarBottom
+import TabBarTop from "../CommonComponents/TabBarTop"
+import TabBarBottom from "../CommonComponents/TabBarBottom"
+import { imgs } from "../../assets/data"
+import axios from 'axios'
+export default {
+	data() {
+		return {
+			leftData: [],
+        	rightData: [],
+        	imgData: []
+		};
+	},
+	created() {
+		this.getList()
+	},
+	methods:{
+
+		getList(){
+			let url = 'http://39.108.245.177:3001/getOpenImg'
+			let params = {
+				page:4
+			}
+			axios.get(url, {params: params}).then((response) => {			
+				this.imgData = response.data.data
+				console.log(response.data.data)
+			})
 		}
+	},
+	components: {
+		TabBarTop,
+		TabBarBottom
 	}
+};
+
 </script>
 <style lang="stylus" scoped>
+.list
+	padding 5px
+.left
+.right
+	width 50%
+	float left
+	.item
+		padding 5px
+		.itemBox
+			background #fff
+			border-radius 4px
+			box-shadow 2px 2px 5px #ddd
+			overflow hidden
+			padding-bottom 5px
+			img
+				width 100%
+				vertical-align top
+				margin-bottom 5px
+			p
+				line-height 20px
+				margin 0 5px
+				font-size 12px
+				&.title
+					height 40px
+					overflow hidden
+				&.otherInfo
+					height 20px
+					color #999
+					.fl
+						float left
+					.fr
+						float right
 </style>
