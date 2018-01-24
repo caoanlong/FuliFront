8daeab6c30c0b85bca52941a0a8f96a3cf872899
coolia <gj@vfu56.com>
2018-01-24 12:01:35 +0800

    调整间距等样式

--- src/components/ImagePart/index.vue
+++ src/components/ImagePart/index.vue
@@ -1,13 +1,14 @@
 <template>
 	<div class="pad-top-36">
 		<tab-bar-top/>
+		<div class="fullBlock"></div>
 		<div class="list clearfix">
 			<div class="left">
 				<div class="item" v-for="(leftItem, index) in imgData" v-if="index % 2 != 0">
 					<div class="itemBox">
 						<img :src="leftItem.url" />
 						<p class="title">新人嫩模杨漫妮居家私房秀傲人豪乳极致美臀诱惑</p>
-						<p class="otherInfo"><span class="fl"><svg-icon icon-class="eye"></svg-icon> 123</span><span class="fr"><svg-icon icon-class="like"></svg-icon> 123</span></p>
+						<p class="otherInfo"><span class="view"><svg-icon icon-class="eye"></svg-icon> 123</span><span><svg-icon icon-class="like"></svg-icon> 123</span></p>
 					</div>
 				</div>
 			</div>
@@ -16,7 +17,7 @@
 					<div class="itemBox">
 						<img :src="rightItem.url" />
 						<p class="title">新人嫩模杨漫妮居家私房秀傲人豪乳极致美臀诱惑</p>
-						<p class="otherInfo"><span class="fl"><svg-icon icon-class="eye"></svg-icon> 123</span><span class="fr"><svg-icon icon-class="like"></svg-icon> 123</span></p>
+						<p class="otherInfo"><span class="view"><svg-icon icon-class="eye"></svg-icon> 123</span><span><svg-icon icon-class="like"></svg-icon> 123</span></p>
 					</div>
 				</div>
 			</div>
@@ -61,8 +62,16 @@ export default {
 
 </script>
 <style lang="stylus" scoped>
+.fullBlock
+	position fixed
+	top 0
+	bottom 0
+	left 0
+	right 0
+	background #f0f0f0
 .list
 	padding 5px
+	position relative
 .left
 .right
 	width 50%
@@ -74,23 +83,23 @@ export default {
 			border-radius 4px
 			box-shadow 2px 2px 5px #ddd
 			overflow hidden
-			padding-bottom 5px
+			padding-bottom 8px
 			img
 				width 100%
 				vertical-align top
-				margin-bottom 5px
+				margin-bottom 8px
 			p
 				line-height 20px
-				margin 0 5px
+				margin 0 10px
 				font-size 12px
 				&.title
 					height 40px
 					overflow hidden
+					color #666
+					margin-bottom 5px
 				&.otherInfo
 					height 20px
-					color #999
-					.fl
-						float left
-					.fr
-						float right
+					color #c7c7c7
+					.view
+						margin-right 10px
 </style>
