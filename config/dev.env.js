31e5db578345739896fe588109e2af6d55f8959f
Caoanlong <caoanlong@gmail.com>
2018-05-22 15:45:23 +0800

    gg

--- config/dev.env.js
+++ config/dev.env.js
@@ -3,5 +3,8 @@ const merge = require('webpack-merge')
 const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
-  NODE_ENV: '"development"'
+	NODE_ENV: '"development"',
+	BASE_API: '"http://47.106.171.37:3100/api-h5"',  // 线上
+	// BASE_API: '"http://localhost:3100/api-h5"',  // 测试
+	IMG_API: '"http://47.106.171.37:3030"',
 })
--- config/prod.env.js
+++ config/prod.env.js
@@ -1,4 +1,6 @@
 'use strict'
 module.exports = {
-  NODE_ENV: '"production"'
+	NODE_ENV: '"production"',
+	BASE_API: '"http://47.106.171.37:3100/api-h5"',  // 线上
+	IMG_API: '"http://47.106.171.37:3030"',
 }
