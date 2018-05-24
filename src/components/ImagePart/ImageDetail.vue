25e5e57408ce9e6a1657f042937d84ba3a05d577
caoanlong <476977540@qq.com>
2018-05-24 08:25:57 +0800

    ggg

--- src/components/ImagePart/ImageDetail.vue
+++ src/components/ImagePart/ImageDetail.vue
@@ -1,21 +1,23 @@
 <template>
-	<div class="pad-top-36">
-		<div class="fullBlock"></div>
+	<pull-to>
 		<div class="imgView">
-			<x-img :src="imgUrl + item" v-for="item in content" :key="item"/>
+			<x-img container=".scroll-container" :src="imgUrl + item" v-for="item in content" :key="item"/>
 		</div>
-		<!--<tab-bar-bottom/> -->
-	</div>
+	</pull-to>
 </template>
 <script type="text/javascript">
-import TabBarBottom from "../CommonComponents/TabBarBottom"
 import request from '../../common/request'
+import PullTo from 'vue-pull-to'
 import { XImg } from 'vux'
 export default {
 	data() {
 		return {
         	content: [],
 		};
+	},
+	components: {
+		PullTo,
+		XImg
 	},
 	created() {
 		this.getDetail()
@@ -33,22 +35,11 @@ export default {
 				this.content = imgInfo.content.split(',')
 			})
 		}
-	},
-	components: {
-		TabBarBottom,
-		XImg
 	}
-};
+}
 
 </script>
 <style lang="stylus" scoped>
-.fullBlock
-	position fixed
-	top 0
-	bottom 0
-	left 0
-	right 0
-	background #f0f0f0
 .imgView
 	padding 5px 5px 0
 	position relative
--- src/components/ImagePart/index.vue
+++ src/components/ImagePart/index.vue
@@ -1,45 +1,58 @@
 <template>
-	<div class="padt">
+	<div class="container" id="container">
 		<tab-bar-top @selectLevel="selectLevel"/>
-		<div class="fullBlock"></div>
-		<div class="list clearfix">
-			<div class="left">
-				<router-link 
-					tag="div" 
-					:to="{name: 'imagedetail', query: {image_id: leftItem.image_id}}" 
-					class="item" 
-					v-for="(leftItem, index) in imgList" 
-					:key="leftItem.image_id" 
-					v-if="index % 2 == 0">
-					<div class="itemBox">
-						<x-img :src="imgUrlMiddle + leftItem.thumbnail.split('/image/uploads')[1]"/>
-						<p class="title">{{leftItem.name}}</p>
-						<p class="otherInfo">
-							<span class="view"><svg-icon icon-class="eye" font-size="14"></svg-icon> {{leftItem.view}}</span>
-							<span><svg-icon icon-class="like"></svg-icon> {{leftItem.like}}</span>
-						</p>
-					</div>
-				</router-link>
+		<pull-to 
+			:bottom-load-method="loadMore" 
+			@bottom-state-change="stateChange">
+			<div class="list">
+				<div class="left">
+					<router-link 
+						tag="div" 
+						:to="{name: 'imagedetail', query: {image_id: leftItem.image_id}}" 
+						class="item" 
+						v-for="(leftItem, index) in imgList" 
+						:key="leftItem.image_id" 
+						v-if="index % 2 == 0">
+						<div class="itemBox">
+							<x-img container=".scroll-container" :src="imgUrlMiddle + leftItem.thumbnail.split('/image/uploads')[1]"/>
+							<p class="title">{{leftItem.name}}</p>
+							<p class="otherInfo">
+								<span class="view"><svg-icon icon-class="eye" font-size="14"></svg-icon> {{leftItem.view}}</span>
+								<span><svg-icon icon-class="like"></svg-icon> {{leftItem.like}}</span>
+							</p>
+						</div>
+					</router-link>
+				</div>
+				<div class="right">
+					<router-link 
+						tag="div" 
+						:to="{name: 'imagedetail', query: {image_id: rightItem.image_id}}" 
+						class="item" 
+						v-for="(rightItem, index) in imgList" 
+						:key="rightItem.image_id" 
+						v-if="index % 2 != 0">
+						<div class="itemBox">
+							<x-img container=".scroll-container" :src="imgUrlMiddle + rightItem.thumbnail.split('/image/uploads')[1]"/>
+							<p class="title">{{rightItem.name}}</p>
+							<p class="otherInfo">
+								<span class="view"><svg-icon icon-class="eye" font-size="14"></svg-icon> {{rightItem.view}}</span>
+								<span><svg-icon icon-class="like"></svg-icon> {{rightItem.like}}</span>
+							</p>
+						</div>
+					</router-link>
+				</div>
 			</div>
-			<div class="right">
-				<router-link 
-					tag="div" 
-					:to="{name: 'imagedetail', query: {image_id: rightItem.image_id}}" 
-					class="item" 
-					v-for="(rightItem, index) in imgList" 
-					:key="rightItem.image_id" 
-					v-if="index % 2 != 0">
-					<div class="itemBox">
-						<x-img :src="imgUrlMiddle + rightItem.thumbnail.split('/image/uploads')[1]"/>
-						<p class="title">{{rightItem.name}}</p>
-						<p class="otherInfo">
-							<span class="view"><svg-icon icon-class="eye" font-size="14"></svg-icon> {{rightItem.view}}</span>
-							<span><svg-icon icon-class="like"></svg-icon> {{rightItem.like}}</span>
-						</p>
-					</div>
-				</router-link>
-			</div>
-		</div>
+			<template slot="bottom-block" slot-scope="props">
+				<div class="bottom-load-wrapper" v-if="isOver">~已经到底了~</div>
+				<div class="bottom-load-wrapper" v-else>
+					<svg-icon :class="{
+						'icon-arrow': props.state === 'trigger',
+						'icon-loading': props.state === 'loading'
+					}" :icon-class="icon"/>
+					{{ props.stateText }}
+				</div>
+			</template>
+		</pull-to>
 		<!--<tab-bar-bottom/> -->
 	</div>
 </template>
@@ -47,83 +60,121 @@
 import TabBarTop from "../CommonComponents/TabBarTop"
 import TabBarBottom from "../CommonComponents/TabBarBottom"
 import request from '../../common/request'
+import PullTo from 'vue-pull-to'
 import { XImg } from 'vux'
 export default {
 	data() {
 		return {
-        	imgList: [],
+			imgList: [],
 			pageIndex: 1,
-			pageSize: 10
-		};
-	},
-	created() {
-	},
-	methods:{
-		selectLevel(level_id) {
-			this.getList(level_id)
-		},
-		getList(level_id){
-			let params = {
-				pageIndex: this.pageIndex,
-				pageSize: this.pageSize,
-				level_id
-			}
-			request({
-				url: '/image/list',
-				params
-			}).then(res => {
-				this.imgList = res.data.data.rows
-			})
+			pageSize: 10,
+			level_id: '',
+			icon: '',
+			isOver: false
 		}
 	},
 	components: {
 		TabBarTop,
 		TabBarBottom,
+		PullTo,
 		XImg
+	},
+	methods:{
+		selectLevel(level_id) {
+			this.level_id = level_id
+			this.getList()
+		},
+		loadMore(loaded) {
+			if (this.isOver) {
+				loaded && loaded('done')
+				return false
+			}
+			this.pageIndex++
+			this.getList(loaded)
+		},
+		getList(loaded){
+			let params = {
+				pageIndex: this.pageIndex,
+				pageSize: this.pageSize,
+				level_id: this.level_id
+			}
+			request({
+				url: '/image/list',
+				params
+			}).then(res => {
+				let list = res.data.data.rows
+				if (list.length == 0) {
+					this.isOver = true
+				}
+				this.imgList.push(...list)
+				loaded && loaded('done')
+			}).catch(err => {
+				loaded && loaded('fail')
+			})
+		},
+		stateChange(state) {
+			if (state === 'pull' || state === 'trigger') {
+			  this.icon = 'arrowup'
+			} else if (state === 'loading') {
+			  this.icon = 'sync'
+			} else if (state === 'loaded-done') {
+			  this.icon = 'check-close-circle-o'
+			}
+		}
 	}
-};
+}
 
 </script>
 <style lang="stylus" scoped>
-.fullBlock
-	position fixed
-	top 0
-	bottom 0
-	left 0
-	right 0
-	background #f0f0f0
-.list
-	padding 5px
-	position relative
-.left
-.right
-	width 50%
-	float left
-	.item
+.bottom-load-wrapper
+	line-height 50px
+	text-align center
+	color #aaa
+.icon-arrow
+	transition .2s
+	transform rotate(180deg)
+.icon-loading
+	transform rotate(0deg)
+	animation-name loading
+	animation-duration 3s
+	animation-iteration-count: infinite
+	animation-direction alternate
+@keyframes loading
+	from {transform: rotate(0deg)}
+	to {transform: rotate(360deg)}
+.container
+	height 100%
+	padding-top 44px
+	.list
 		padding 5px
-		.itemBox
-			background #fff
-			border-radius 4px
-			box-shadow 2px 2px 5px rgba(0,0,0,.2)
-			overflow hidden
-			padding-bottom 8px
-			img
-				width 100%
-				vertical-align top
-				margin-bottom 8px
-			p
-				line-height 20px
-				margin 0 10px
-				font-size 12px
-				&.title
-					height 40px
+		.left,.right
+			width 50%
+			float left
+			.item
+				padding 5px
+				.itemBox
+					background #fff
+					border-radius 4px
+					box-shadow 2px 2px 5px rgba(0,0,0,.2)
 					overflow hidden
-					color #666
-					margin-bottom 5px
-				&.otherInfo
-					height 20px
-					color #c7c7c7
-					font-family Helvetica,HelveticaNeue,Droid Sans
-					.view
-						margin-right 10px
+					padding-bottom 8px
+					img
+						width 100%
+						vertical-align top
+						margin-bottom 8px
+					p
+						line-height 20px
+						margin 0 10px
+						font-size 12px
+						&.title
+							height 40px
+							overflow hidden
+							color #666
+							margin-bottom 5px
+						&.otherInfo
+							height 20px
+							color #c7c7c7
+							font-family Helvetica,HelveticaNeue,Droid Sans
+							.view
+								margin-right 10px
 </style>
